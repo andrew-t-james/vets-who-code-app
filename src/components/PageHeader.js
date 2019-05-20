ff125ba150cc65e73d172c19e41946c4f51fea23
Andrew James <ajames20@gmail.com>
2019-05-20 17:49:00 -0500

    Add PageHeader component to leverage StaticQuery

---
+++ src/components/PageHeader.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link, StaticQuery, graphql } from 'gatsby'
+
+const PageHeader = ({ title }) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          file(relativePath: { eq: "this_is_us.png" }) {
+            childImageSharp {
+              fixed(width: 1200) {
+                ...GatsbyImageSharpFixed
+
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <header
+          className="inner-header overlay grey text-center slim-bg "
+          style={{
+            backgroundImage: `url(${data.file.childImageSharp.fixed.src})`,
+            backgroundPositionY: 'bottom',
+          }}
+        >
+          <div className="overlay-01" />
+          <div className="container">
+            <h2 className="text-center text-uppercase">{title}</h2>
+            <div className="breadcrumb">
+              <Link to="/">Home</Link>
+              <span>/</span>
+              <Link to="/contact" className="page-active">
+                Contact Us
+              </Link>
+            </div>
+          </div>
+        </header>
+      )}
+    />
+  )
+}
+
+export default PageHeader
